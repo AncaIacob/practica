2d93591cc12a9a71e0d4c30f89af4fadae6485bb
iacob <iacob@DESKTOP-EL42J7M.localdomain>
2020-07-09 23:04:57 +0300

    Initial commmit

---
+++ LargeObject.cpp
@@ -0,0 +1,20 @@
+
+#include "LargeObject.hpp"
+#include <cstddef>
+#include <cstdlib>
+
+LargeObject::LargeObject()
+{
+    for(size_t i = 0; i < LARGE_OBJECT_SIZE; ++i)
+    {
+        m_data[i] = std::rand();
+    }
+}
+
+LargeObject::~LargeObject()
+{
+    for(size_t i = 0; i < LARGE_OBJECT_SIZE; ++i)
+    {
+        m_data[i] = 0;
+    }
+}
