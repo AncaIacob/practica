1be03e038d68a9bb65505e580ed5a79da16a8c3e
iacob <iacob@DESKTOP-EL42J7M.localdomain>
2020-07-09 22:43:00 +0300

    Initial commmit

---
+++ Vector.hpp
@@ -0,0 +1,31 @@
+#include<cstddef>
+
+
+class Vector
+{
+    public :
+
+     Vector();
+     Vector(std::size_t capacity);
+    ~Vector();
+     std::size_t getSize();
+     std::size_t getCapacity();
+    void insert(std::size_t idx,int element);
+    void pushFront(int element);
+    void pushBack(int element);
+    int getElement(std::size_t idx);
+    int getFront();
+    int getBack();
+    void setElement(std::size_t idx, int elemnet);
+    void setFront(int element);
+    void setBack(int element);
+    void clear();
+    bool empty();
+    
+
+    private :
+    std::size_t m_size;
+    std::size_t m_capacity;
+    int* m_data;
+    void reserve(std::size_t newCapacity);
+};
