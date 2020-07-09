1dd03a0a09b10b7f35a806306bb942885ce7f6f5
iacob <iacob@DESKTOP-EL42J7M.localdomain>
2020-07-09 23:32:25 +0300

    Initial commmit

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main
 {
-   Vector vec(10);
+   Vector vec(4);
    vec.pushBack(0);
    vec.pushBack(1);
   vec.pushBack(2);
   vec.pushBack(3);
-  vec.pushBack(4);
+  vec.pushBack(4)
   vec.pushFront(5);
   std::cout<< vec.getFront()<< " " << vec.getBack() << " " << vec.getElement(1) <<" " << vec.getCapacity()<<" " <<std::endl;
 
