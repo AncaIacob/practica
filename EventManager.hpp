5179d40c8070c519222e38c8b9d8e951c9692fdf
Anca <ancaiacob_98@yahoo.com>
2020-08-05 10:29:37 +0300

    fixed some problems

--- EventManager.hpp
+++ EventManager.hpp
@@ -3,12 +3,29 @@
 #include "EventPayload.hpp"
 #include "EventType.hpp"
 #include "Vector.hpp"
+#include "Functional.hpp"
+#include "iostream"
 #include <functional>
 class EventManager
 {
   public:
-    void subscribe(EventType type, THandler handler);
-    void fire(EventType type, EventPayload payload);
+    void subscribe(EventType type, THandler handler)
+    {
+        if(!m_handlers.contains(type) 
+        {
+            m_handlers[type] = {};
+        }
+
+        m_handlers[type].push_back(handler);
+    }
+    void fire(eventType type, EventPayload payload)
+    {
+        for (auto& handler : m_handlers[type])
+        {
+            handler(payload);
+        }
+    }
+
   private:
     std::function<void(const EventPayload&)> THandler;
     map<EventType, vector<THandler>> m_handlers;
